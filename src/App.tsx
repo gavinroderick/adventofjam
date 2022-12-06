b90ba003be16ebc4a821d56ae4b1f3e3278b31d8
Gavin Roderick <gavin.roderick26@gmail.com>
2022-12-06 10:19:53 +0000

    feat: change how the data works

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
 import "./App.css";
 import Calendar from "./components/Calendar/Calendar";
-import image1 from "./assets/img/jam/01.jpg";
-import image2 from "./assets/img/jam/02.jpg";
-import image3 from "./assets/img/jam/03.jpg";
-import image4 from "./assets/img/jam/04.jpg";
+import Days from "./JamData";
 
 function App() {
   const jamData: Calendar = {
     title: "Advent of Jam",
     year: 2022,
-    windows: [
-      { day: "1", title: "Strawberry", imageUrl: image1, rating: 10 },
-      {
-        day: "2",
-        title: "Grapefruit & Dragonfruit",
-        imageUrl: image2,
-        rating: 8,
-      },
-      {
-        day: "3",
-        title: "Mirabelle Plum & Linden Jam",
-        imageUrl: image3,
-        rating: 6.5,
-      },
-      { day: "4", title: "Apricot & Bergamot", imageUrl: image4, rating: 2 },
-    ],
+    windows: Days,
   };
   return (
     <div className="App">
-      {/* <Calendar
-        title="Advent of CSS"
-        year={cssData.year}
-        windows={cssData.windows}
-      /> */}
       <Calendar
         title={jamData.title}
         year={jamData.year}
